b540388a5704d4fd2e9b9d0e9c654a89f6cdc30c
jun.du@intel.com <jun.du@intel.com>
2024-11-27 19:09:43 -0800

    update

--- inference.py
+++ inference.py
@@ -176,19 +176,19 @@ def main():
     with torch.no_grad():
         if args.precision == "float16" and args.device == "cuda":
             print("---- Use autocast fp16 cuda")
-            with torch.autocast(enabled=True, dtype=torch.float16):
+            with torch.autocast(enabled=True, dtype=torch.float16, device_type=args.device):
                 inference(args)
         elif args.precision == "float16" and args.device == "xpu":
             print("---- Use autocast fp16 xpu")
-            with torch.autocast(enabled=True, dtype=torch.float16, cache_enabled=True):
+            with torch.autocast(enabled=True, dtype=torch.float16, cache_enabled=True, device_type=args.device):
                 inference(args)
         elif args.precision == "bfloat16" and args.device == "cpu":
             print("---- Use autocast bf16 cpu")
-            with torch.autocast(enabled=True, dtype=torch.bfloat16):
+            with torch.autocast(enabled=True, dtype=torch.bfloat16, device_type=args.device):
                 inference(args)
         elif args.precision == "bfloat16" and args.device == "xpu":
             print("---- Use autocast bf16 xpu")
-            with torch.autocast(dtype=torch.bfloat16):
+            with torch.autocast(enabled=True, dtype=torch.bfloat16, device_type=args.device):
                 inference(args)
         else:
             print("---- no autocast")
